606220e85c8d2fdf98ad59578618818967c6c8af
Samuel Steele <ssteele@bizstream.com>
2021-01-25 22:47:52 -0500

    added xml comments

--- src/StaticWebAssetsStorage/src/Strings.cs
+++ src/StaticWebAssetsStorage/src/Strings.cs
@@ -1,9 +1,11 @@
 namespace BizStream.Extensions.Kentico.Xperience.StaticWebAssetsStorage
 {
 
+    /// <summary> Constant strings. </summary>
     public static class Strings
     {
 
+        /// <summary> The <c>BasePath</c> of the Kentico RCL assembly. </summary>
         public const string KenticoMvcRCLBasePath = "_content/Kentico.Content.Web.Rcl";
 
     }
