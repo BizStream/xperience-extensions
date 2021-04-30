eda47fd47336510a3f444f0940b3b82dde270fb9
Samuel Steele <ssteele@bizstream.com>
2021-04-30 13:28:12 -0400

    added AtRootLevel IDocumentQuery extension

--- src/DocumentEngine/src/IDocumentQueryExtensions.cs
+++ src/DocumentEngine/src/IDocumentQueryExtensions.cs
@@ -1,4 +1,7 @@
-﻿using CMS.DocumentEngine;
+﻿using System;
+using BizStream.Extensions.Kentico.Xperience.DataEngine;
+using CMS.DataEngine;
+using CMS.DocumentEngine;
 
 namespace BizStream.Extensions.Kentico.Xperience.DocumentEngine
 {
@@ -6,6 +9,23 @@ namespace BizStream.Extensions.Kentico.Xperience.DocumentEngine
     /// <summary> Extensions to <see cref="IDocumentQuery"/> and <see cref="IDocumentQuery{TQuery, TObject}"/>. </summary>
     public static class IDocumentQueryExtensions
     {
+
+        /// <summary> Filters the query to pages of the given PageType that exist at the root of the Content Tree. </summary>
+        /// <param name="query"> The query to filter. </param>
+        /// <typeparam name="TNode"> The PageType to filter by. </typeparam>
+        public static TQuery AtRootLevel<TQuery, TNode>( this IDocumentQuery<TQuery, TNode> query )
+            where TQuery : IDocumentQuery<TQuery, TNode>, new()
+            where TNode : TreeNode, new()
+        {
+            if( query == null )
+            {
+                throw new ArgumentNullException( nameof( query ) );
+            }
+
+            var typedQuery = query.GetTypedQuery();
+            return typedQuery.NestingLevel( 1 )
+                .ResetOrderBy( nameof( TreeNode.NodeOrder ), OrderDirection.Descending );
+        }
 
         /// <summary> Get the name of the Sql alias used for the query based on the type of query. </summary>
         /// <see cref="Strings.DocumentQueryAliasName"/>
