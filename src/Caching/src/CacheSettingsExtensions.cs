bb20ca0b2c1d36d778b4150a48b3ce7d3e47ba42
Samuel Steele <ssteele@bizstream.com>
2021-10-20 09:55:02 -0400

    add xml docs

--- src/Caching/src/CacheSettingsExtensions.cs
+++ src/Caching/src/CacheSettingsExtensions.cs
@@ -3,8 +3,12 @@ using CMS.Helpers;
 
 namespace BizStream.Extensions.Kentico.Xperience.Caching
 {
+    /// <summary> Extensions to <see cref="CacheSettings"/> to assist with Fluent configuration of settings. </summary>
     public static class CacheSettingsExtensions
     {
+        /// <summary> Configure the <see cref="CacheSettings"/> dependencies. </summary>
+        /// <param name="settings"> The <see cref="CacheSettings"/> to configure. </param>
+        /// <param name="dependency"> The configured dependency object. </param>
         public static CacheSettings WithCacheDependency( this CacheSettings settings, CMSCacheDependency dependency )
         {
             if( settings is null )
@@ -16,6 +20,9 @@ namespace BizStream.Extensions.Kentico.Xperience.Caching
             return settings;
         }
 
+        /// <summary> Configure the <see cref="CacheSettings"/> dependencies. </summary>
+        /// <param name="settings"> The <see cref="CacheSettings"/> to configure. </param>
+        /// <param name="configure"> A delegate that configures a <see cref="CMSCacheDependency"/> for the <paramref name="settings"/>. </param>
         public static CacheSettings WithCacheDependency( this CacheSettings settings, Action<CMSCacheDependency> configure )
         {
             if( settings is null )
